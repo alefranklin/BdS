9487862699c39fb620e387227c5d2f8281285d3c
Paguz <pagottomanuel@gmail.com>
2020-09-05 21:20:13 +0200

    fix css print

--- footer.php
+++ footer.php
@@ -1,8 +1,10 @@
 <div id="footer">
     <div id="footer-info">
-        <span>Via Trieste, 63, 35121 Padova PD</span> - 
+        <span>Via Trieste, 63, 35121 Padova PD</span> -
         <span><a href="tel:5559933902">555-9933902</a></span>
     </div>
-    <a href="admin.php">Login Pannello di amministrazione</a>
-    <a href="#menu">Torna al menu</a>
+    <div id="footer-link">
+      <a href="admin.php">Login Pannello di amministrazione</a>
+      <a href="#menu">Torna al menu</a>
+    </div>
 </div>
