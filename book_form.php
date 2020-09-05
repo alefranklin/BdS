78ee599bc6fd451111843025fff733e9b9467c6e
Paguz <pagottomanuel@gmail.com>
2020-09-05 16:49:24 +0200

    fix

--- book_form.php
+++ book_form.php
@@ -7,42 +7,61 @@
 <div id="form-prenotazione">
     <form method="post" action="book.php" onsubmit="return checkBookForm()">
         <fieldset>
-            <label for="nome">Nome</label>
-            <input type="text" id="nome" name="nome"/>
-            <label for="cognome">Cognome</label>
-            <input type="text" id="cognome" name="cognome"/>
-            <label for="data">Data</label>
-            <select name="data" id="data">
-                <?php
-                $week = getNextWeekFromTomorrow();
-                for($i = 0; $i < count($week); $i++){
-                // Inserisce il giorno nella select e lo disabilita se in quel giorno ci sono già 8 prenotazioni (giorno pieno)
-                ?>
-                <option value="<?= $week[$i]?>" <?= checkDayAvailability($week[$i],$db_data) ? "" : "disabled" ?>><?= $week[$i]?></option>
-                <?php } ?>
-            </select>
-            <label for="ora">Ora</label>
-            <select name="ora" id="ora">
-            <?php
-                $hours = getHours();
-                for($i = 0; $i < 8; $i++){?>
-                <option value="<?= $hours['start'][$i]?>"><?= $hours['start'][$i]?></option>
-                <?php } ?>
-            </select>
-            <span>Seleziona i servizi che desideri prenotare (massimo 4)</span>
-            <?php
-                    $i = 0;
-                    while ($row = $services->fetch_assoc()){
-                ?>
-            <label for="servizio<?= $i; ?>"><?= $row['nome'];?></label>
-            <input type="checkbox" name="servizi[]" value="<?= $row['nome'];?>" id="servizio<?= $i; ?>"/>
-                <?php
-                    $i += 1;
-                    }
-            ?>
+            <div class="form-element">
+              <label for="nome">Nome</label>
+              <input type="text" id="nome" name="nome"/>
+            </div>
 
-            <label for="email">E-mail</label>
-            <input type="text" id="email" name="email"/>
+            <div class="form-element">
+              <label for="cognome">Cognome</label>
+              <input type="text" id="cognome" name="cognome"/>
+            </div>
+
+            <div class="form-element">
+              <label for="data">Data</label>
+              <select name="data" id="data">
+                  <?php
+                  $week = getNextWeekFromTomorrow();
+                  for($i = 0; $i < count($week); $i++){
+                  // Inserisce il giorno nella select e lo disabilita se in quel giorno ci sono già 8 prenotazioni (giorno pieno)
+                  ?>
+                  <option value="<?= $week[$i]?>" <?= checkDayAvailability($week[$i],$db_data) ? "" : "disabled" ?>><?= $week[$i]?></option>
+                  <?php } ?>
+              </select>
+            </div>
+
+            <div class="form-element">
+              <label for="ora">Ora</label>
+              <select name="ora" id="ora">
+              <?php
+                  $hours = getHours();
+                  for($i = 0; $i < 8; $i++){?>
+                  <option value="<?= $hours['start'][$i]?>"><?= $hours['start'][$i]?></option>
+                  <?php } ?>
+              </select>
+            </div>
+
+            <div class="form-element">
+              <span>Seleziona i servizi che desideri prenotare (massimo 4)</span>
+              <?php
+                      $i = 0;
+                      while ($row = $services->fetch_assoc()){
+                  ?>
+              <div class="form-element">
+                <input type="checkbox" name="servizi[]" value="<?= $row['nome'];?>" id="servizio<?= $i; ?>"/>
+                <label for="servizio<?= $i; ?>"><?= $row['nome'];?></label>
+              </div>
+                  <?php
+                      $i += 1;
+                      }
+              ?>
+            </div>
+
+            <div class="form-element">
+              <label for="email">E-mail</label>
+              <input type="text" id="email" name="email"/>
+            </div>
+
             <input type="submit" value="Prenota"/>
         </fieldset>
     </form>
