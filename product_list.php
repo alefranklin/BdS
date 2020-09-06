d5895d255b00d40c88b6673bb1cc43d2798ff84b
Alessandro Franchin <alessandro.franchin@hotmail.it>
2020-09-06 17:20:13 +0200

    fix form filtri

--- product_list.php
+++ product_list.php
@@ -2,17 +2,21 @@
 <form action="catalogo.php" method="get">
     <fieldset>
         <legend>Filtri:</legend>
-        <label for="tipo">Tipo</label>
-        <select name="tipo" id="tipo">
-            <option value="both">Tutti</option>
-            <option value="strumento">Strumento</option>
-            <option value="accessorio">Accessorio</option>
-        </select>
-        <label for="prezzo">Ordina prezzo</label>
-        <select name="prezzo" id="prezzo">
-            <option value="ASC">Dal pi&ugrave; basso</option>
-            <option value="DESC">Dal pi&ugrave; alto</option>
-        </select>
+        <div class="form-element">
+            <label for="tipo">Tipo</label>
+            <select name="tipo" id="tipo">
+                <option value="both">Tutti</option>
+                <option value="strumento">Strumento</option>
+                <option value="accessorio">Accessorio</option>
+            </select>
+        </div>
+        <div class="form-element">
+            <label for="prezzo">Ordina prezzo</label>
+            <select name="prezzo" id="prezzo">
+                <option value="ASC">Dal pi&ugrave; basso</option>
+                <option value="DESC">Dal pi&ugrave; alto</option>
+            </select>
+        </div>
         <input type="submit" value="Filtra"/>
     </fieldset>
 </form>
