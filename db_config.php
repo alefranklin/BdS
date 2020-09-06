386caff70405575b4d90fbea00ab385791b5ac39
Matteo- <matteopagotto2@gmail.com>
2020-09-06 16:05:02 +0200

    fix

--- db_config.php
+++ db_config.php
@@ -2,5 +2,5 @@
     $db_data['server_name'] = "localhost";
     $db_data['name'] = "alfranch";
     $db_data['username'] = "root";
-    $db_data['password'] = "316212529";
+    $db_data['password'] = "";
 ?>
